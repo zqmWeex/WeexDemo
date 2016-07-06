35031a0445455907edaa2761df5915042676935b
laznrbfe <laznrbfe@163.com>
2016-07-06 10:37:21 +0800

    0706am

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,68 @@
-require('webpack')
-require('weex-loader')
+var path = require('path');
+var fs = require('fs');
+var webpack = require('webpack');
+var loader = require('weex-loader');
+var scripter = require('weex-scripter');
+var styler = require('weex-styler');
+var templater = require('weex-templater');
 
-var path = require('path')
+loader.useScripter(scripter);
+loader.useStyler(styler);
+loader.useTemplater(templater);
+
+var entry = {};
+
+function walk(dir, root) {
+  var directory = path.join(__dirname, root, dir);
+  fs.readdirSync(directory)
+    .forEach(function(file) {
+      var fullpath = path.join(directory, file);
+      var stat = fs.statSync(fullpath);
+      var extname = path.extname(fullpath);
+      if (stat.isFile() &&
+             (extname === '.we' || extname === '.js')) {
+        var name = path.join(root, 'build', dir, path.basename(file, extname));
+        entry[name] = fullpath + '?entry=true';
+      } else if (stat.isDirectory() &&
+                  file !== 'build') {
+        var subdir = path.join(dir, file);
+        walk(subdir, root);
+      }
+    });
+}
+walk('./', 'examples');
+walk('./', 'test');
 
 module.exports = {
-  entry: {
-    main: path.join(__dirname, 'src', 'main.we?entry=true')
-  },
-  output: {
-    path: 'dist',
+  entry: entry,
+  output : {
+    path: __dirname,
     filename: '[name].js'
   },
   module: {
     loaders: [
       {
         test: /\.we(\?[^?]+)?$/,
-        loaders: ['weex-loader']
+        loader: 'weex'
+      },
+      {
+        test: /\.js(\?[^?]+)?$/,
+        loader: 'weex?type=script'
+      },
+      {
+        test: /\.css(\?[^?]+)?$/,
+        loader: 'weex?type=style'
+      },
+      {
+        test: /\.html(\?[^?]+)?$/,
+        loader: 'weex?type=tpl'
       }
     ]
   }
 }
+
+// //生成原生调试二维码
+// var qrcode = require('qrcode-terminal');
+// qrcode.generate("http://"+getIPAddress()+":12580/dist/main.js");
+//
+// console.log("\r\n按住ctrl点击右侧地址打开应用--->http://localhost:12580\r\n");
